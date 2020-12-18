4b265b7e859bba027bd3ede07b6b11daae63d9c2
Tran Thao Van <45513997+vantranthao@users.noreply.github.com>
2020-12-18 21:54:45 +0700

    th4_2

--- 2.cpp
+++ 2.cpp
@@ -1,22 +1,96 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 using namespace std;
-void nhapData (double& a, double& b, double& c) {
-	cout << "Canh thu nhat: "; cin >> a;
-	cout << "Canh thu hai: "; cin >> b;
-	cout << "Canh thu ba: "; cin >> c;
-}
-double cal (double& a, double& b, double& c, double p, double chuVi, double dienTich) {
-	if (a+b>c && a+c>b && c+b>a) {
-		p = (a+b+c)/2;
-		chuVi = 2*p;
-		dienTich = sqrt(p*(p-a)*(p-b)*(p-c));
-		cout << chuVi << endl << dienTich;
-	} else cout << "Sai roi!";
-}
-
+class Employee {
+	protected:
+		string name;
+		int age;
+		double salary;
+	public:
+		Employee() {
+			name = "Nguyen Van A";
+			age = 20;
+			salary = 1000;
+		}
+		Employee (string name1, int age1, double sal1) {
+			name = name1;
+			age = age1;
+			salary = sal1;
+		}
+		// get
+		string getName() {
+			return name;
+		}
+		int getAge() {
+			return age;
+		}
+		double getSalary() {
+			return salary;
+		}
+		// set
+		string setName(string n) {
+			name = n;
+		}
+		int setAge(int a) {
+			age = a;
+		}
+		double setSalary(double s) {
+			salary = s;
+		}
+		// func
+		void selfIntroduce() {
+			cout << "I am an employee";
+		}
+		void output() {
+			cout << "----------------\n";
+			//cout << name << endl << age << endl << salary;
+			cout << "Ten: " << name << endl << "Tuoi: " << age << endl << "Luong: " << salary << endl;
+			selfIntroduce(); cout << endl;
+		}
+};
+class Doctor : public Employee { // ke thua
+	private:
+		double fee; // phi kham benh
+		string speciality; // chuyen mon bsi
+	public:
+		Doctor():Employee() {
+			fee = 10;
+			speciality = "Mo bung";
+		}
+		Doctor(string ten, int tuoi, double luong, double phi, string chuyenMon):Employee(ten, tuoi, luong){
+			
+			fee = phi;
+			speciality = chuyenMon;
+		}
+		// get
+		double getFee() {
+			return fee;
+		}
+		string getSpeciality() {
+			return speciality;
+		}
+		// set
+		double setFee( double f) {
+			fee = f;
+		}
+//		string speciality(string s) {
+//			speciality = s;
+//		}
+		void selfIntroduce() { // how to inheritate ? 
+			cout << "I am a doctor";
+		}
+		void output() {
+			cout << "-----------------\n";
+			cout << "Ten bsi: " << name << endl << "Tuoi: " << age << endl << "Luong: " << salary << endl;
+			cout << "Phi kham: " << fee << endl << "Chuyen mon: " << speciality << endl;
+			selfIntroduce(); cout << endl;
+		}
+};
 int main() {
-	double a,b,c,p, dienTich, chuVi;
-	nhapData(a,b,c);
-	cal(a,b,c,p, chuVi, dienTich);
+	Employee a0; a0.output(); // fix cung!
+	Employee a("Van thao", 21, 1200), b;
+	a.output();
+	
+	Doctor a1("Lisa", 19, 1000, 120, "Phu san");
+	a1.output();
 }
